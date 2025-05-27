2935a32b01a38c9815c084b30a2c98bbb0bd4b44
Mohamed Ayoub Ahmed Abdelaziz Khabiry <135470965+mohameA337@users.noreply.github.com>
2025-05-27 23:59:01 +0300

    update main

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ import io
 
 app = FastAPI()
 
-model = YOLO(r"C:\Users\moham\Documents\GitHub\-Pothole-Detection-with-YOLO-and-FastAPI-Deployment\notebooks\yolov5_project\content\runs\detect\train\weights\best.pt")  
+model_nano = YOLO(r"C:\Users\moham\Documents\GitHub\-Pothole-Detection-with-YOLO-and-FastAPI-Deployment\notebooks\yolov5_project (1)\content\runs\detect\train\weights\best.pt")
+model_small = YOLO(r"C:\Users\moham\Documents\GitHub\-Pothole-Detection-with-YOLO-and-FastAPI-Deployment\notebooks\yolov5_project (1)\content\runs\detect\train2\weights\best.pt")
 
-
-def predict_image(image: Image.Image) -> Image.Image:
+def predict_and_annotate(model, image: Image.Image) -> Image.Image:
     results = model(image)
-    annotated_frame = results[0].plot()  
-    return Image.fromarray(annotated_frame)
+    annotated = results[0].plot()  
+    return Image.fromarray(annotated)
 
-@app.post("/predict")
-async def predict_image_route(file: UploadFile = File(...)):
-  
+@app.post("/predict/nano")
+async def predict_nano(file: UploadFile = File(...)):
     image = Image.open(file.file).convert("RGB")
+    predicted = predict_and_annotate(model_nano, image)
 
-    predicted_image = predict_image(image)
-
- 
     buffer = io.BytesIO()
-    predicted_image.save(buffer, format="PNG")
+    predicted.save(buffer, format="PNG")
     buffer.seek(0)
+    return StreamingResponse(buffer, media_type="image/png")
 
+@app.post("/predict/small")
+async def predict_small(file: UploadFile = File(...)):
+    image = Image.open(file.file).convert("RGB")
+    predicted = predict_and_annotate(model_small, image)
+
+    buffer = io.BytesIO()
+    predicted.save(buffer, format="PNG")
+    buffer.seek(0)
     return StreamingResponse(buffer, media_type="image/png")
